73bb8bf1ce322a9f87a6998f20bc5f39f9119836
Azkal Adzkia <adzkiaazkal90@gmail.com>
2026-05-18 14:52:10 +0700

    pembuatan model dto

--- src/main/java/com/deploy/pertemuan11/model/dto/RegisterRequest.java
+++ src/main/java/com/deploy/pertemuan11/model/dto/RegisterRequest.java
@@ -1,23 +1,12 @@
 package com.deploy.pertemuan11.model.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
     private String username;
     private String password;
     
-    // getters and setters
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String nama;
+    private String alamat;
 }
