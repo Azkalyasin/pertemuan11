3e15b416490b780f99e778c84300c254b6e3ac0e
Azkal Adzkia <adzkiaazkal90@gmail.com>
2026-05-18 14:51:40 +0700

    pembuatan profile model

--- src/main/java/com/deploy/pertemuan11/model/Profil.java
+++ src/main/java/com/deploy/pertemuan11/model/Profil.java
@@ -1,13 +1,25 @@
 package com.deploy.pertemuan11.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profil {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
+
+    private String nama;
+    private String alamat;
+    
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
